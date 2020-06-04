aecc122df1107d30f4f5e4df0d92e255b48917b0
Jonas <jonas.luft@hcu-hamburg.de>
2020-06-04 14:36:54 +0200

    renamed test functions

--- test_full.py
+++ test_full.py
@@ -2,11 +2,11 @@ import requests
 
 root_url = "http://127.0.0.1:5000/"
 
-def life_test():
+def test_life():
     response = requests.get(root_url)
     assert(response.status_code == 200)
 
-def login_pos_test():
+def test_login_pos():
     username = "testuser"
     password = "blubb"
     data = {
@@ -16,7 +16,7 @@ def login_pos_test():
     response = requests.post(root_url+"login",json=data)
     assert(response.status_code == 200)
 
-def login_neg_pw_test():
+def test_login_neg_pw():
     username = "testuser"
     password = "falsches_passwort"
     data = {
@@ -26,7 +26,7 @@ def login_neg_pw_test():
     response = requests.post(root_url+"login",json=data)
     assert(response.status_code == 401)
 
-def login_neg_user_test():
+def test_login_neg_user():
     username = "kein_user"
     password = "passwort"
     data = {
@@ -36,7 +36,7 @@ def login_neg_user_test():
     response = requests.post(root_url+"login",json=data)
     assert(response.status_code == 401)
 
-def register_pos_test():
+def test_register_pos():
     username = "neuer_user"
     password = "neues_passwort"
     data = {
@@ -50,7 +50,7 @@ def register_pos_test():
     print("deleting test entry",response.json())
     deleteUser(response.json()["user_id"])
 
-def register_neg_test():
+def test_register_neg():
     username = "testuser"
     password = "neues_passwort"
     data = {
@@ -60,7 +60,7 @@ def register_neg_test():
     response = requests.post(root_url+"register",json=data)
     assert(response.status_code == 403)
 
-def layer_test():
+def test_layer():
     data = {
         "userid":"e3b205abbda908571fa09d99bac58ef9",
         "layer":"features_buildings"
@@ -69,7 +69,7 @@ def layer_test():
     assert(response.status_code == 200)
     print(response.json()["type"])
 
-def layerdata_test():
+def test_layerdata():
     query = "features/0/id"
     data = {
         "userid":"e3b205abbda908571fa09d99bac58ef9",
@@ -79,7 +79,7 @@ def layerdata_test():
     assert(response.status_code == 200)
     print(response.text)
 
-def layerchange_nonexistant_test():
+def test_layerchange_nonexistant():
     query = "nonexistant_layer/features/0/id"
     data = {
         "userid":"e3b205abbda908571fa09d99bac58ef9",
@@ -88,7 +88,7 @@ def layerchange_nonexistant_test():
     response = requests.post(root_url+"addLayerData/"+query,json=data)
     assert(response.status_code == 400)
 
-def layerchange_add_test():
+def test_layerchange_add():
     import random
     query = "test_layer/data/1/state"
     data = {
@@ -98,7 +98,7 @@ def layerchange_add_test():
     response = requests.post(root_url+"addLayerData/"+query,json=data)
     assert(response.status_code == 200)
 
-def layerchange_add_test2():
+def test_layerchange_add2():
     query = "test_layer2/"
     data = {
         "userid":"e3b205abbda908571fa09d99bac58ef9",
@@ -109,7 +109,7 @@ def layerchange_add_test2():
     import os
     os.remove("data/user/e3b205abbda908571fa09d99bac58ef9/test_layer2.json")
 
-def layerchange_all_test():
+def test_layerchange_all():
     query = "test_layer/"
     data = {
         "userid":"e3b205abbda908571fa09d99bac58ef9",
@@ -119,28 +119,28 @@ def layerchange_all_test():
     assert(response.status_code == 200)
 
 if __name__ == "__main__":
-    print("life_test")
-    life_test()
-    print("login_pos_test")
-    login_pos_test()
-    print("login_neg_pw_test")
-    login_neg_pw_test()
-    print("login_neg_user_test")
-    login_neg_user_test()
-    print("register_pos_test")
-    register_pos_test()
-    print("register_neg_test")
-    register_neg_test()
+    print("test_life")
+    test_life()
+    print("test_login_pos")
+    test_login_pos()
+    print("test_login_neg_pw")
+    test_login_neg_pw()
+    print("test_login_neg_user")
+    test_login_neg_user()
+    print("test_register_pos")
+    test_register_pos()
+    print("test_register_neg")
+    test_register_neg()
 
-    print("layer_test")
-    layer_test()
-    print("layerdata_test")
-    layerdata_test()
-    print("layerchange_nonexistant_test")
-    layerchange_nonexistant_test()
-    print("layerchange_add_test")
-    layerchange_add_test()
-    print("layerchange_add_test2")
-    layerchange_add_test2()
-    print("layerchange_all_test")
-    layerchange_all_test()
+    print("test_layer")
+    test_layer()
+    print("test_layerdata")
+    test_layerdata()
+    print("test_layerchange_nonexistant")
+    test_layerchange_nonexistant()
+    print("test_layerchange_add")
+    test_layerchange_add()
+    print("test_layerchange_add2")
+    test_layerchange_add2()
+    print("test_layerchange_all")
+    test_layerchange_all()
