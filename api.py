2fc475812952ac8580977c5a374eab873f2d186f
andredaa <post.for.andre@gmail.com>
2020-08-11 10:38:39 +0200

    Move ABM filters to separate file

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, abort
 from flask_cors import CORS
 from database_connector import makeUser,checkPass,getUserId,getLayer,changeLayer,checkUser
+from abm_filters import apply_time_filters, apply_agent_filters
 
 app = Flask(__name__)
 CORS(app)
@@ -145,37 +146,12 @@ def getAbmData(query):
         abort(404)  # no matching result found
 
     if time_filters:
-        start_time = time_filters["start_time"]
-        end_time = time_filters["end_time"]
-        for agent_data in abm_result["data"]:
-            agent_data["timestamps"] = list(filter(lambda x: start_time < x < end_time, agent_data["timestamps"]))
-        abm_result["data"] = list(filter(lambda agent: agent["timestamps"], abm_result["data"]))
+        apply_time_filters(abm_result, time_filters)
 
     if agent_filters:
-        relevant_agents_data = []
-        for agent_data in abm_result["data"]:
-            relevant_agent = True
+        apply_agent_filters(abm_result, agent_filters)
 
-            for key, value in agent_filters.items():
-                try:
-                    if key == "modes":  # modes contains dict of allowed transport modes {"bicycle":true, "car":false}
-                        if not value[agent_data["agent"]["mode"]]:
-                            relevant_agent = False
-                            break
-                    elif not agent_data["agent"][key] in [value, "unknown"]:  # matching value or "unknown" is relevant
-                        relevant_agent = False
-                        break
-                except Exception as e:
-                    print("filter criteria does not match target data")
-                    print(e)
-                    abort(400)
-            if relevant_agent:
-                relevant_agents_data.append(agent_data)
-
-        return {"data": relevant_agents_data}
-
-    else:  # return the entire result dataset, if no filters applied
-        return {"data": abm_result["data"]}
+    return {"data": abm_result["data"]}
 
 
 @app.route("/addLayerData/<path:query>", methods = ['POST'])
